369972aacdedd716646a9cd7069e9496b3773531
PawarSunil97 <pawarsunil281297@gmail.com>
2025-06-09 21:51:39 +0530

    adding dot env file for firebase config

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,20 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCmstEfcCjsP3XhKNTetE_AbQORhmeuf8o",
-  authDomain: "real-time-board-d54db.firebaseapp.com",
-  databaseURL: "https://real-time-board-d54db-default-rtdb.firebaseio.com",
-  projectId: "real-time-board-d54db",
-  storageBucket: "real-time-board-d54db.firebasestorage.app",
-  messagingSenderId: "853477267834",
-  appId: "1:853477267834:web:3616eadf0c55e7ab411ec3",
-  measurementId: "G-3EGWX6K41N"
+  apiKey:process.env.FIREBASE_API_KEY,
+  authDomain:process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL:process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket:process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId:process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
